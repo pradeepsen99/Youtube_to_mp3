3d96ef91a69ed3a71b40afe77ca5cc375617fb2a
H4sher <h4sh@H4sher.local>
2020-11-28 16:38:34 -0600

    Configured setuptools for dependancies.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Youtube_to_mp3",
+    version="0.1.3a",
+    packages=find_packages(),
+    include_package_data=True,
+    python_requires=">=3.6",
+    entry_points={"console_scripts": ["audiodl=scripts.main:convert"]},
+    install_requires=[
+        "click>=7.1.2,<8.0.0",
+        "pydub>=0.24.1,<1.0.0",
+    ]
+)
