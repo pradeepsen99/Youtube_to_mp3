8fefa69f614fce3f9d1ceecd4b107ebadfa2a2a9
Arnas Amankavicius <arnas.jonas.a@gmail.com>
2020-12-27 16:11:01 -0600

    Cleaned up,added author information,build script.

--- scripts/main.py
+++ scripts/main.py
@@ -11,12 +11,15 @@ from conversionjob import ConversionJob
 from song_downloader import download_from_link, download_from_file
 from logger import Logger
 
+from __version__ import __version__
+
 def setupParser():
     parser = argparse.ArgumentParser(description="Download and convert YouTube video content to specified audio formats")
     parser.add_argument('songs_file', metavar='songs_file', type=str, help='Path to the file containing all of the YouTube links')
     parser.add_argument('export_path', metavar='export_path', type=str, help='Path where to convert the downloaded audio')
     parser.add_argument('-w', '--workers', default=5, type=int, help='Amount of processes to create for audio conversion (default: 5)')
     parser.add_argument('-v', '--verbose', action='store_true', help='Enable verbose for debugging purposes.')
+    parser.add_argument('--version', action="version", version="%(prog)s {}".format(__version__), help="Display version information")
 
     return parser.parse_args()
 
@@ -27,14 +30,11 @@ def convert():
 
     c = Config(
         verbose=args.verbose,
-        # yt_dl_path=Path.home().as_posix() + "/Downloads/Downloaded_Videos",
         yt_dl_path=args.export_path,
-        # convert_path=Path.home().as_posix() + "/Downloads/Converted_Videos",
         convert_path=args.export_path,
         workers=args.workers,
         out_format=".mp3"
     )
-    #link_file = Path.home().as_posix() + "/Downloads/links.txt"
     link_file = args.songs_file
 
     logger = c.logger
@@ -42,6 +42,5 @@ def convert():
     logger.verbose("Output : {}".format(c.convert_path), c.verbose)
     logger.verbose("Workers : {}".format(c.workers), c.verbose)
 
-    # download_from_link("testlink", c)
     download_from_file(file_path=link_file, config=c)
     conv(config=c, input_directory=c.yt_dl_path, output_directory=c.convert_path, output_format=c.out_format, workers=c.workers)
